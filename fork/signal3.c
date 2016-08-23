5f8485e59e1f2e80a7c0ff684b47d542fd9d969d
Sam Huo <hzk8656511@126.com>
2016-08-23 22:40:59 +0800

    DO: signal() and sigaction() experiment

---
+++ fork/signal3.c
@@ -0,0 +1,26 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void my_func(int sign_no)
+{
+    if (sign_no == SIGINT)
+    {
+        printf("I have get SIGINT\n");
+    }
+    else if (sign_no == SIGQUIT)
+    {
+        printf("I have get SIGQUIT\n");
+    }
+}
+
+int main()
+{
+
+    printf("Waiting for signal SIG or SIGQUIT ... \n");
+    signal(SIGINT, my_func);
+    signal(SIGQUIT, my_func);
+    pause();
+    exit(0);
+}
+
